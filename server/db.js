49fa5c14228b01b19c145a765a8dcb17187c59e4
Zerrrks <58989968+Zerrrks@users.noreply.github.com>
2022-03-12 23:45:04 -0500

    team commit
    xd

--- server/db.js
+++ server/db.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   host: "localhost",
   user: "postgres",
-  password: "kthl8822",
+  password: "user123",
   port: 5432,
-  database: "jwtauth"
+  database: "authtodolist"
 });
 
 module.exports = pool;
